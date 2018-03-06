7d928e70f10d4d908ac61a12ff6d6411f62218ed
Omar Elemary <elemaryo@mcmaster.ca>
2018-03-06 07:32:24 -0500

    Unfunctional Kruskal

---
+++ src/algo/Edge.java
@@ -0,0 +1,72 @@
+/*
+ * Code referenced from Algorithms and Data Structures
+ * By Robert Sedgewick and Kevin Wayne
+ */
+
+
+public class Edge implements Comparable<Edge> {
+
+	private final int v; // one vertex
+	private final int w; // the other vertex
+	private final double weight; // edge weight
+	
+//Constructor for edge class
+	
+	public Edge(int v, int w, double weight){
+		this.v = v;
+		this.w = w;
+		this.weight = weight;
+	}
+	
+//Accessors for verticies and weight
+
+	public double weight(){ 
+		return weight; 
+	}
+
+	public int either(){ 
+		return v; 
+	}
+	
+	public int other(int vertex){
+		if (vertex == v) {
+			return w;
+		}
+		
+		else if (vertex == w) {
+			return v;
+		}
+		
+		else {
+			throw new RuntimeException("Inconsistent edge");
+		}
+	}
+//Comparable function for edges
+	public int compareTo(Edge that){
+		
+		if (this.weight() < that.weight()) {
+			return -1;
+		}
+		
+		else if (this.weight() > that.weight()) {
+			return +1;
+		}
+		
+		else { 
+			return 0;
+		}
+	}
+	
+	public String toString(){ 
+		
+		return String.format("%d-%d %.2f", v, w, weight);
+		
+	}
+
+}
+
+
+
+
+
+
---
+++ src/algo/UF.java
@@ -0,0 +1,61 @@
+
+public class UF {
+	
+    private int[] parent;  // parent[i] = parent of i
+    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
+    private int count;     // number of components
+
+//Constructor of empty UF data structure
+    
+    public UF(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        count = n;
+        parent = new int[n];
+        rank = new byte[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+ 
+//Checks if an element p is in the connected component
+    
+    public int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];    // path compression by halving
+            p = parent[p];
+        }
+        return p;
+    }
+    
+//Returns the number of components
+    
+    public int count() {
+        return count;
+    }
+    
+//Returns true if the the two elements are in the same component/connected.
+    
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+//Connect two elements
+    
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make root of smaller rank point to root of larger rank
+        if      (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
+        else {
+            parent[rootQ] = rootP;
+            rank[rootP]++;
+        }
+        count--;
+    }
+    
+    
+}

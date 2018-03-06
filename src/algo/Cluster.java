4b0a72bce189e4ad8bd75e9dfcaf4343369c1553
le-michael <lem12@mcmaster.ca>
2018-03-06 14:49:04 -0500

    Animation

--- src/algo/Cluster.java
+++ src/algo/Cluster.java
@@ -1,21 +1,38 @@
 package algo;
 
+import java.awt.Color;
+import java.util.Random;
 
 public class Cluster {
 
 	private Cord centroid;
+	private int size;
+	private Color c;
+	
 
+	public Cluster(float x,float y) {
+		Random rand = new Random();
+		centroid = new Cord(x,y);
+		size = 1;
+		c = new Color(rand.nextFloat(),rand.nextFloat(),rand.nextFloat());
+	}
+	
+	public Color getColor() {
+		return c;
+	}
+	
+	public int getSize() {
+		return size;
+	}
 	
 	public Cord getCenter() {
 		return centroid;
 	}
 	
-	public Cluster(float x,float y) {
-		centroid = new Cord(x,y);
-	}
 	
 	public void insertCord(Cord n) {
 		insertCord(centroid,n);
+		size++;
 	}
 	
 	private void insertCord(Cord head,Cord n) {
--- src/algo/DisplayClusters.java
+++ src/algo/DisplayClusters.java
@@ -15,61 +15,91 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 class Surface extends JPanel implements ActionListener {
 
-	Cluster [] clusters;
+	/*Enviroment Variables*/
+	private Cluster [] clusters;
 	
+	/*Draw Variables*/
+	private int dispCurr;
+	private int maxSize=0;
 
+	
+	
     public Surface(Cluster[] c) {
     	clusters = c;
+    	for(int i=0; i< clusters.length;i++) {
+    		maxSize += clusters[i].getSize();
+    	}
+    	dispCurr = 1;
+    	
     }
     private void doDrawing(Graphics g) {
     	Random rand = new Random();
         Graphics2D g2d = (Graphics2D) g;
 
-       
-        
+       boolean done = false;
+       int count = 0;
         for(int i =0; i < clusters.length;i++) {
         	
-        	g2d.setPaint(new Color(rand.nextFloat(),rand.nextFloat(),rand.nextFloat()));
+        	g2d.setPaint(clusters[i].getColor());
         	Cord tempCord = clusters[i].getCenter();
         	while (tempCord != null) {
-        		g2d.fillOval((int)tempCord.getX(), (int)tempCord.getY(), 10, 10);
+        		g2d.fillOval((int)tempCord.getX(), (int)tempCord.getY(), 6, 6);
         		tempCord = tempCord.getNext();
+        		if (++count == dispCurr) {
+        			done = true;
+        			break;
+        		}
         	}
+        	if (done) break;
         }
        
     }
 
+
     @Override
     public void paintComponent(Graphics g) {
-
         super.paintComponent(g);
         doDrawing(g);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    	if (dispCurr < maxSize) dispCurr ++;
         repaint();
     }
+    
+    
+    
+
+
 }
 
 public class DisplayClusters extends JFrame {
-
+	private Timer time;
+	private int REFRESH = 100; 
+	
 	Cluster [] clusters;
     public DisplayClusters(Cluster[] c) {
     	clusters = c;
-        initUI();
+    	initUI();
+        
     }
 
     private void initUI() {
 
         final Surface surface = new Surface(clusters);
         add(surface);
+        
+        time = new Timer(REFRESH, surface);
+        time.start();
 
 
         setTitle("Clusters");
-        setSize(500, 500);
+        setSize(750, 750);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }

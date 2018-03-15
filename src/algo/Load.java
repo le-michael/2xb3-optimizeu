56dfb224b1e149bd0564b6e7a15eae0cbca52680
Necried <duttonl@mcmaster.ca>
2018-03-14 20:48:07 -0400

    initial normalize added

--- src/algo/Load.java
+++ src/algo/Load.java
@@ -13,20 +13,44 @@ public class Load {
 	public Load() {
 		String f = "./data/uber-raw-data-may14.csv";
 		String line;
+		Cord minima = new Cord(Integer.MAX_VALUE, Integer.MAX_VALUE);
+		Cord maxima = new Cord(Integer.MIN_VALUE, Integer.MIN_VALUE);
 		try (BufferedReader br = new BufferedReader(new FileReader(f))){
 			line = br.readLine();
 			line = br.readLine();
 			String [] temp = line.split(",");
 			data = new Cord(Double.parseDouble(temp[1]),Double.parseDouble(temp[2]));
 			Cord pCord = data;
+			if (pCord.getY() > pCord.getY())
+				maxima.setY(pCord.getY());
+			if (pCord.getX() > maxima.getX())
+				maxima.setX(pCord.getX());
+			if (pCord.getY() < minima.getY())
+				minima.setY(pCord.getY());
+			if (pCord.getX() < minima.getX())
+				minima.setX(pCord.getX());
 			while ((line = br.readLine())!= null) {
 				temp = line.split(",");
 				pCord.setNext(new Cord(Double.parseDouble(temp[1]),Double.parseDouble(temp[2])));
 				pCord = pCord.getNext();
+				if (pCord.getY() > pCord.getY())
+					maxima.setY(pCord.getY());
+				if (pCord.getX() > maxima.getX())
+					maxima.setX(pCord.getX());
+				if (pCord.getY() < minima.getY())
+					minima.setY(pCord.getY());
+				if (pCord.getX() < minima.getX())
+					minima.setX(pCord.getX());
 			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		
+		Cord pCord = data;
+		for (Cord i = pCord; i != null; i = i.getNext()) {
+			i.setX((i.getX()- minima.getX())/(maxima.getX()-minima.getX()));
+			i.setY((i.getY()- minima.getY())/(maxima.getY()-minima.getY()));
 		}
 	}
 		
